513f4870d8ee10dcb6279f9609253529e3af7515
SSIS students <98946372+ssi5@users.noreply.github.com>
2022-05-12 22:14:50 +0700

    script started at boot

---
+++ code/launcher.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/admin/Documents
+sudo python MakeBlock.py
+cd /
